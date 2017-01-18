84778f3a0bfaf164339f91827a1d83c776403ad1
weirui222 <congcongli@live.com>
2017-01-17 18:31:13 -0800

    Basic calculator

--- js/app.js
+++ js/app.js
@@ -1,1 +1,72 @@
-console.log('app.js was loaded.');
+// console.log('app.js was loaded.');
+angular.module('App', [])
+.controller('appCtrl', ['$scope', function($scope) {
+  $scope.previousNum = 0;
+  $scope.currentNum = 0;
+  $scope.waitingForNum = true;
+  $scope.operator = '';
+
+  $scope.click = function(num) {
+    if ($scope.waitingForNum) {
+      $scope.currentNum = num;
+      $scope.waitingForNum = false;
+    } else {
+      $scope.currentNum = $scope.currentNum * 10 + num;
+    }
+    console.log('num', num);
+  };
+
+  $scope.clear = function() {
+    $scope.currentNum = 0;
+  };
+
+  $scope.plus = function() {
+    $scope.calc();
+    $scope.operator = '+';
+  };
+
+  $scope.minus = function() {
+    $scope.calc();
+    $scope.operator = '-';
+  };
+
+  $scope.times = function() {
+    $scope.calc();
+    $scope.operator = '*';
+  };
+
+  $scope.divide = function() {
+    $scope.calc();
+    $scope.operator = '/';
+  };
+
+  $scope.equals = function() {
+    $scope.calc();
+    $scope.operator = '';
+  }
+
+  $scope.calc = function() {
+    switch ($scope.operator) {
+      case '+':
+        $scope.currentNum = $scope.previousNum + $scope.currentNum;
+        break;
+
+      case '-':
+        $scope.currentNum = $scope.previousNum - $scope.currentNum;
+        break;
+
+      case '*':
+        $scope.currentNum = $scope.previousNum * $scope.currentNum;
+        break;
+
+      case '/':
+        $scope.currentNum = $scope.previousNum / $scope.currentNum;
+        break;
+
+      default:
+    }
+
+    $scope.previousNum = $scope.currentNum;
+    $scope.waitingForNum = true;
+  };
+}]);
